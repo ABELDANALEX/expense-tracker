9f3345d38e7c2b59649617971da2c0d63b42460b
patrickgerrr <rspratheek@gmail.com>
2025-06-16 00:37:04 +0530

    completed login and signup pages and added functionality to the buttons on homepage

--- frontend/src/views/Login/Login.jsx
+++ frontend/src/views/Login/Login.jsx
@@ -1,7 +1,27 @@
-export default function Login(){
-    return(
-        <>
-        Login
-        </>
-    )
+import React from "react";
+import "./Login.css";
+
+export default function Login() {
+  return (
+    <>
+      <div className="login-container">
+        <div className="card">
+          <div className="header">
+            <div className="txt">LogIn</div>
+          </div>
+          <div className="form">
+            <div>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div>
+              <input type="password" placeholder="Password" />
+            </div>
+          </div>
+          <div className="submit-container">
+            <div className="submit">Login</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- frontend/src/views/Signup/Signup.jsx
+++ frontend/src/views/Signup/Signup.jsx
@@ -1,7 +1,32 @@
-export default function Signup(){
-    return(
-        <>
-        Signup
-        </>
-    )
+import "./Signup.css";
+
+export default function Signup() {
+  return (
+    <>
+      <div className="signup-container">
+        <div className="card">
+          <div className="header">
+            <div className="txt">SignUp</div>
+          </div>
+          <div className="form">
+            <div>
+              <input type="text" placeholder="Username" />
+            </div>
+            <div>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div>
+              <input type="password" placeholder="Password" />
+            </div>
+            <div>
+              <input type="password" placeholder="Confirm Password" />
+            </div>
+          </div>
+          <div className="submit-container">
+            <div className="submit">SignUp</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
