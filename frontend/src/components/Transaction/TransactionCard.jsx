1176aa7104875ad9af5b52f1f6c92786ac611ab6
patrickgerrr <rspratheek@gmail.com>
2025-06-17 03:35:01 +0530

    transaction card added

---
+++ frontend/src/components/Transaction/TransactionCard.jsx
@@ -0,0 +1,16 @@
+import './Transaction.css'
+
+export default function TransactionCard(props){
+    return(
+        <>
+        <div className="transaction-card">
+            {/* <div>{props.date}</div>
+            <div>{props.title}</div>
+            <div>{props.amount}</div> */}
+            <div>date</div>
+            <div>title</div>
+            <div>amount</div>
+        </div>
+        </>
+    )
+}
