cc28d87a0d2e63cc04d1359d528855dbe9bc2ce6
patrickgerrr <rspratheek@gmail.com>
2025-06-17 22:21:08 +0530

    added authentication(login and signup) backend

---
+++ backend/routes/auth.js
@@ -0,0 +1,9 @@
+const authRouter = require('express').Router();
+const authController = require('../controllers/authController');
+const { authLimiter } = require('../middleware/rateLimiter');
+
+authRouter.post('/signup', authLimiter, authController.signup);
+
+authRouter.post('/login', authLimiter, authController.login);
+
+module.exports = authRouter;
