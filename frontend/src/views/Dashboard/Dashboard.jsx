565210dbf855b1070170b349dc96c38f04ffef3f
abeldanalex <abeldanalex635@gmail.com>
2025-06-17 11:31:22 +0530

    Added the frontend for sort

--- frontend/src/views/Dashboard/Dashboard.jsx
+++ frontend/src/views/Dashboard/Dashboard.jsx
@@ -5,6 +5,14 @@ export default function Dashboard(){
     const firstTime = true; //whether first time login or not
     const username = "John"; //get it from login
     const [balance, setBalance] = useState(100000);
+    const [sortOption, setSortOption] = useState("latest")
+
+
+    const handleSortChange = (e) => {
+        const selected = e.target.value;
+        setSortOption(selected)
+        //other stuff goes here
+    }
 
     const commafy = (number) => {
           return new Intl.NumberFormat('en-IN').format(number);
@@ -27,8 +35,18 @@ export default function Dashboard(){
                     Current Balance: <span className="current-balance" id="current-balance">₹ {commafy(balance)}</span>
                 </div>
                 <hr className="dashboard-rule-1" id="dashboard-rule-1"/>
+                <div className="history-headings-container" id="history-headings-container">
+                    <button className="add-newexpense-button" id="add-newexpense-button"><div>+</div></button>
+                    <div><i>Transaction History</i></div>
+                    <select className="transaction-history-order" name="transaction-history-order" id="transaction-history-order" value={sortOption} onChange={handleSortChange}>
+                        <option value="latest">By date(latest first)</option>
+                        <option value="oldest">By date(oldest first)</option>
+                        <option value="high">By expense(descending)</option>
+                        <option value="low">By expense(ascending)</option>
+                    </select>
+                </div>
                 <div className="transaction-history" id="transaction-history">
-
+                    
                 </div>
                 
             </div>
