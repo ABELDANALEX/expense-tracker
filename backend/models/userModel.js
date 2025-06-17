609682ae22a9dd09b19929f877d48ee781ebf9bc
patrickgerrr <rspratheek@gmail.com>
2025-06-17 17:57:36 +0530

    added user model and db credentials

---
+++ backend/models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    amount:{
+        type:Number,
+        required:true,
+    }
+})
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
