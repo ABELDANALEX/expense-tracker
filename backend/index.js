b5ed0ee1e21cbe5bb6810a68c14528d7daaa2896
patrickgerrr <rspratheek@gmail.com>
2025-06-17 22:21:08 +0530

    added authentication(login and signup) backend

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const cors=require('cors')
+const mongoConnection=require('./config/mongodb')
+const authRouter=require('./routes/auth')
+const app = express();
+const PORT= process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors({
+    origin: '*'
+}))
+
+mongoConnection();
+
+
+app.use('/auth', authRouter);
+
+
+app.listen(PORT, () =>
+    console.log(`Server is running on port ${PORT}`)
+);
