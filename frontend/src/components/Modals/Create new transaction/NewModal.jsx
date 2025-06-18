759a9eafc2190e44b6e474c30e7e4f2d77e64005
patrickgerrr <rspratheek@gmail.com>
2025-06-18 18:21:10 +0530

    added category to the modal and note

--- frontend/src/components/Modals/Create new transaction/NewModal.jsx
+++ frontend/src/components/Modals/Create new transaction/NewModal.jsx
@@ -1,57 +1,88 @@
-import './NewModal.css'
-import { set, useForm } from 'react-hook-form';
-import { useRef } from 'react';
+import "./NewModal.css";
+import { set, useForm } from "react-hook-form";
+import { useRef } from "react";
 
-export default function NewModal({onClose}) {
+export default function NewModal({ onClose }) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
 
-    const{
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors }
-        } = useForm();
-
-
-     const modalRef = useRef();
+  const modalRef = useRef();
 
   const handleBackdropClick = (e) => {
     if (modalRef.current === e.target) {
       onClose();
     }
-  }
+  };
   const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-  const onSubmit=async (data)=>{
-    await delay(2000)
-    console.log(data)
-}
-  
+  const onSubmit = async (data) => {
+    await delay(2000);
+    console.log(data);
+  };
 
   return (
     <>
-      <div className="modal-container" ref={modalRef} onClick={handleBackdropClick}>
+      <div
+        className="modal-container"
+        ref={modalRef}
+        onClick={handleBackdropClick}
+      >
         <form className="modal-card" onSubmit={handleSubmit(onSubmit)}>
           <div className="newModal-header" id="newModal-header">
-            <div className="txt" id="txt">Create transaction</div>
+            <div className="txt" id="txt">
+              Create transaction
+            </div>
           </div>
           <div className="transaction-form" id="transaction-form">
             <div>
               <label htmlFor="title">Title</label>
-              <input {...register("title")}type="text" />
+              <input {...register("title",{ required: true })} type="text" />
             </div>
             <div>
               <label htmlFor="amount">Amount</label>
-              <input  {...register("amount")}type="number" />
+              <input {...register("amount",{ required: true })} type="number" />
             </div>
             <div>
               <label htmlFor="date">Date</label>
-              <input {...register("date")} type="date" />
+              <input {...register("date",{ required: true })} type="date" />
+            </div>
+            <div>
+              <label htmlFor="note">Note</label>
+              <input {...register("note")} type="text" />
+            </div>
+            <div>
+                <label htmlFor="">Category</label>
+              <select className="transaction-category" {...register('category', { required: true })}>
+                <option value="groceries">Groceries</option>
+                <option value="medical">Medical</option>
+                <option value="personal">Personal</option>
+                <option value="entertainment">Entertainment</option>
+                <option value="bills">Bills</option>
+                <option value="transport">Transport</option>
+                <option value="other">Other</option>
+              </select>
             </div>
           </div>
           <div className="submit-container" id="submit-container">
-            
-            <div> <button className="submit" id="create-submit" type="submit" >Create</button></div>
-            <div><button className='submit' id="cancel" type="button" onClick={onClose}>Cancel</button></div>
-            
+            <div>
+              {" "}
+              <button className="submit" id="create-submit" type="submit">
+                Create
+              </button>
+            </div>
+            <div>
+              <button
+                className="submit"
+                id="cancel"
+                type="button"
+                onClick={onClose}
+              >
+                Cancel
+              </button>
+            </div>
           </div>
         </form>
       </div>
