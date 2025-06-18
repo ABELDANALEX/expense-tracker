a59e120f727d2e1dfde92fabfa053f1bea22fbd8
abeldanalex <abeldanalex635@gmail.com>
2025-06-18 09:46:28 +0530

    Added transition effects to make stuff look smoother

--- frontend/src/components/Transaction/TransactionCard.jsx
+++ frontend/src/components/Transaction/TransactionCard.jsx
@@ -7,9 +7,9 @@ export default function TransactionCard(props){
             {/* <div>{props.date}</div>
             <div>{props.title}</div>
             <div>{props.amount}</div> */}
-            <div>12/3/34</div>
-            <div>Biryani</div>
-            <div>₹200</div>
+            <div className="transaction-card-date" id="transaction-card-date">12/3/34</div>
+            <div className="transaction-card-title" id="transaction-card-title">Biryani</div>
+            <div className="transaction-card-amount" id="transaction-card-amount">₹200</div>
         </div>
         </>
     )
