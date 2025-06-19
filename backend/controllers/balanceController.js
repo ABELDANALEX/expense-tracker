3c534ae05570f34febb00f9c911d378048a30e77
patrickgerrr <rspratheek@gmail.com>
2025-06-19 19:25:12 +0530

    added getting balance api by user id

--- backend/controllers/balanceController.js
+++ backend/controllers/balanceController.js
@@ -1,5 +1,20 @@
 const User=require('../models/userModel')
 
+exports.getBalance=async(req,res)=>{
+    const id=req.params.id
+    try{
+        const user=await User.findById(id)
+        if(!user){
+            return res.status(404).json({message:'User not found'})
+        }
+        const balance=user.balance
+        return res.status(200).send({message:"Balance fetched successfully",balance})
+    }catch(error){
+        console.error("Error fetching balance",error.message)
+        return res.status(500).send({message:"Error fetching balance"})
+    }
+}
+
 exports.updateBalance=async(req,res)=>{
     const {id,amount}=req.body
     try{
@@ -12,6 +27,6 @@ exports.updateBalance=async(req,res)=>{
         return res.status(200).send({message:'Balance updated successfully',updatedUser})
     }catch(error){
         console.error("Error updating balance",error.message)
-        return res.status(400).json({message:'Error updating balance'})
+        return res.status(400).send({message:'Error updating balance'})
     }
 }
--- backend/routes/balance.js
+++ backend/routes/balance.js
@@ -1,6 +1,8 @@
 const balanceRouter = require('express').Router();
 const balanceController = require('../controllers/balanceController')
 
+balanceRouter.get('/:id',balanceController.getBalance)
+
 balanceRouter.patch('/',balanceController.updateBalance)
 
 module.exports=balanceRouter
