aedfbdd742a4cf2b6179a49c0f148ed323d57313
patrickgerrr <rspratheek@gmail.com>
2025-06-20 06:44:57 +0530

    signup integrated and slight changes to signup.jsx and css

--- frontend/src/views/Signup/Signup.jsx
+++ frontend/src/views/Signup/Signup.jsx
@@ -1,72 +1,119 @@
+import { useState } from "react";
 import "./Signup.css";
+
+import axios from "axios";
+
 import { useNavigate } from "react-router-dom";
 
 export default function Signup() {
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [balance, setBalance] = useState(undefined);
+  const [step, setStep] = useState(1); //step 1 for creds and step 2 for taking the initial balance
 
-  const handleSubmit = (e) => {
+  const handleCredSubmit = async (e) => {
     e.preventDefault();
-    const name = document.getElementById("username")?.value.trim();
-    const email = document.getElementById("email")?.value.trim()
-    const pwd = document.getElementById("password")?.value
-    const c_pwd = document.getElementById("c_password")?.value
 
-    if (!name || !email || !pwd || !c_pwd){
-      window.alert("Please fill in all fields.")
-      return
+    if (!username || !email || !password || !confirmPassword) {
+      window.alert("Please fill in all fields.");
+      return;
     }
 
-    if (pwd != c_pwd){
-      window.alert("Passwords do not match")
-      return
+    if (password !== confirmPassword) {
+      window.alert("Passwords do not match");
+      return;
     }
 
-    alert("Signup Successful. Please login now to access the website")
-    //set balance to -1. DO a check in Dashboard - if balance is -1 prompt user to enter initial balance, else show current balance
-    navigate('/login')
-  }
+    setStep(2);
+  };
+
+  const handleBalanceSubmit = async (e) => {
+    e.preventDefault();
+    if (!balance || isNaN(balance) || Number(balance) < 0){
+      window.alert("Please enter a valid balance");return
+    }
+    const data={username,email,password,balance:Number(balance)}
+    try{
+      const response = await axios.post(`/auth/signup`,data)
+      console.log(response.data)
+      navigate("/login");
+      alert("Sign up successful")
+    }catch(error){
+      console.error(error);
+    }
+  };
+
   return (
     <>
-      <form className="signup-container" id="signup-container" onSubmit={handleSubmit}>
+      <form
+        className="signup-container"
+        id="signup-container"
+        onSubmit={step === 1 ? handleCredSubmit : handleBalanceSubmit}
+      >
         <div className="signup-card" id="signup-card">
           <div className="signup-header" id="signup-header">
             <div className="txt">Sign Up</div>
           </div>
-          <div className="signup-form" id="signup-form">
-            <div>
-              <label htmlFor="username">Username:</label>
-              <input id="username" name="username" type="text" placeholder="Username" />
+          {step === 1 && (
+            <div className="signup-form" id="signup-form">
+              <div>
+                <label htmlFor="username">Username:</label>
+                <input
+                  value={username}
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  onChange={(e)=>{setUsername(e.target.value)}}
+                />
+              </div>
+              <div>
+                <label htmlFor="email">Email:</label>
+                <input
+                  value={email}
+                  name="email"
+                  type="email"
+                  placeholder="someone@enterprisename.com"
+                  onChange={(e)=>{setEmail(e.target.value)}}
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Password:</label>
+                <input
+                  value={password}
+                  name="password"
+                  type="password"
+                  placeholder="Enter Password"
+                  onChange={(e)=>{setPassword(e.target.value)}}
+                />
+              </div>
+              <div>
+                <label htmlFor="c_password">Confirm Password:</label>
+                <input
+                  value={confirmPassword}
+                  name="c_password"
+                  type="password"
+                  placeholder="Re-Enter Password"
+                  onChange={(e)=>{setConfirmPassword(e.target.value)}}
+                />
+              </div>
             </div>
-            <div>
-              <label htmlFor="email">Email:</label>
-              <input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="someone@enterprisename.com"
-              />
+          )}
+          {step === 2 && (
+            <div className="signup-form">
+              <label htmlFor="balance" className="balance-label" >Initial Balance (inr)</label>
+              <input type="number" value={balance} name="balance" onChange={(e)=>{setBalance(e.target.value)}} placeholder="Enter initial balance" />
             </div>
-            <div>
-              <label htmlFor="password">Password:</label>
-              <input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="Enter Paswword"
-              />
-            </div>
-            <div>
-              <label htmlFor="c_password">Confirm Password:</label>
-              <input
-                id="c_password"
-                name="c_password"
-                type="password"
-                placeholder="Re-Enter Password"
-              />
-            </div>
+          )}
+          <div className="signup-form">
+            
           </div>
           <div className="signup-submit-container" id="signup-submit-container">
-            <button className="signup-submit" id="signup-submit" type="submit">Sign Up</button>
+            <button className="signup-submit" id="signup-submit" type="submit">
+              {step===1?"Next":"Sign Up"}
+            </button>
           </div>
           <div className="login-link-container" id="login-link-container">
             Already have an account? &nbsp;
