aa0f359882fbdbe58c8166dc393d04dc871b5138
patrickgerrr <rspratheek@gmail.com>
2025-06-21 15:21:45 +0530

    time shown in transaction and improved appearance of the card

--- frontend/src/components/Transaction/TransactionCard.jsx
+++ frontend/src/components/Transaction/TransactionCard.jsx
@@ -3,10 +3,16 @@ import './Transaction.css'
 export default function TransactionCard(props){
     const dateString =props.date;
     const onlyDate = new Date(dateString).toISOString().split('T')[0];
+    const timez=new Date(dateString).toISOString().split('T')[1];
+    const time = timez.slice(0,5);
     return(
         <>
         <div key={props.key} className="transaction-card">
-            <div className="transaction-card-date" id="transaction-card-date">{onlyDate}</div>
+            <div className="transaction-card-ist">
+                <div className="transaction-card-date" id="transaction-card-date">{onlyDate}</div>
+                <div className="transaction-card-time" >{time}</div> 
+            </div>
+            
             <div className="transaction-card-title" id="transaction-card-title">{props.title}</div>
             <div className="transaction-card-category" id="transaction-card-category">{props.category}</div>
             <div className="transaction-card-amount" id="transaction-card-amount">₹{props.amount}</div>
