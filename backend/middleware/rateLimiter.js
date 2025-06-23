1f66ebfb7e6e5d59bcbe2b8ccb49c79906bc91ae
patrickgerrr <rspratheek@gmail.com>
2025-06-23 17:32:50 +0530

    minor rate change

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,8 +1,8 @@
 const rateLimit = require('express-rate-limit');
 
 const authLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, //15 mins window
-    max: 15, // limit to 5 requests per window of duration
+    windowMs: 2 * 60 * 1000, //2 mins window
+    max: 5, // limit to 5 requests per window of duration
     message: {
         success: false,
         message: 'Too many attempts, please try again after sometime.'
